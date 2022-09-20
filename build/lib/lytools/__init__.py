63aeb3bcd22e01d49315b681e54ba43e97775a0a
Yang-Mini <leeyang1991@gmail.com>
2022-09-20 20:52:31 +0800

    0.0.66

--- build/lib/lytools/__init__.py
+++ build/lib/lytools/__init__.py
@@ -1,11 +1,8 @@
 # coding='utf-8'
-__version__ = '0.0.65'
+__version__ = '0.0.66'
 
-# from ._lytools import *
-# from outdated import warn_if_outdated
+from ._lytools import *
 from outdated import check_outdated
-# warn_if_outdated("lytools", __version__)
-# check_outdated("lytools", __version__)
 is_outdated, latest = check_outdated("lytools", __version__)
 if is_outdated:
     print("The package lytools is out of date. Your version is %s, the latest is %s." % (__version__, latest))
