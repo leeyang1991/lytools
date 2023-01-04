5fd50c087fdb3a8222f0c5810a594dfb4c9d203b
Yang-Mini <leeyang1991@gmail.com>
2023-01-05 00:12:42 +0800

    0.0.84

--- build/lib/lytools/__init__.py
+++ build/lib/lytools/__init__.py
@@ -1,5 +1,5 @@
 # coding='utf-8'
-__version__ = '0.0.83'
+__version__ = '0.0.84'
 
 from ._lytools import *
 from outdated import check_outdated
--- lytools/_lytools.py
+++ lytools/_lytools.py
@@ -697,7 +697,6 @@ class Tools:
         pass
 
     def plot_colors_palette(self, cmap):
-        plt.figure()
         sns.palplot(cmap)
 
     def group_consecutive_vals(self, in_list):
@@ -1722,6 +1721,54 @@ class Tools:
         for name,group in df_groupby:
             df_group_dict[name] = group
         return df_group_dict
+
+    def color_map_choice(self):
+        color_map_list = [
+            'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap',
+            'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd',
+            'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2',
+            'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r',
+            'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r',
+            'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral',
+            'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd',
+            'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg',
+            'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper',
+            'copper_r', 'crest', 'crest_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'flare', 'flare_r',
+            'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar',
+            'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r',
+            'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r',
+            'icefire', 'icefire_r', 'inferno', 'inferno_r', 'magma', 'magma_r', 'mako', 'mako_r',
+            'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism',
+            'prism_r', 'rainbow', 'rainbow_r', 'rocket', 'rocket_r', 'seismic', 'seismic_r', 'spring', 'spring_r',
+            'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r',
+            'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted',
+            'twilight_shifted_r', 'viridis', 'viridis_r', 'vlag', 'vlag_r', 'winter', 'winter_r'
+        ]
+        color_number = 0
+        for c in color_map_list:
+            if c.endswith('_r'):
+                continue
+            color_number += 1
+        fig = plt.figure()
+        flag = 0
+        for c in color_map_list:
+            if c.endswith('_r'):
+                continue
+            flag += 1
+            ax = fig.add_subplot(10, 9, flag)
+            print(c)
+            pal = sns.color_palette(c,n_colors=11)
+            n = len(pal)
+            ax.imshow(np.arange(n).reshape(1, n),
+                      cmap=mpl.colors.ListedColormap(list(pal)),
+                      interpolation="nearest",aspect='auto')
+            plt.xticks([])
+            plt.yticks([])
+            plt.title(c)
+        plt.tight_layout()
+        plt.show()
+
+        pass
 
 class SMOOTH:
     '''
